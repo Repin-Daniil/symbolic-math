0c86079d60fca35a2663fb55b146278a1c53bce7
DaniilR <daniil.r4@yandex.ru>
2024-04-10 07:28:51 +0300

    Tests for unary-operation's GetNumericResult

--- tests/unary_operations_test.cpp
+++ tests/unary_operations_test.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 #include "math/algebra/expressions/operands/number.h"
 #include "math/algebra/expressions/operands/variable.h"
@@ -25,9 +26,11 @@ TEST_CASE("-5", "UnaryMinus") {
 
   CHECK(result->GetInfix(0) == "-5");
   CHECK(result->GetRPN() == "5 ~");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(-5, 1e-5));
 
   CHECK(derivative->GetInfix(0) == "-0");
   CHECK(derivative->GetRPN() == "0 ~");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinAbs(0, 1e-5));
 }
 
 TEST_CASE("-x", "UnaryMinus") {
@@ -38,9 +41,11 @@ TEST_CASE("-x", "UnaryMinus") {
 
   CHECK(result->GetInfix(0) == "-x");
   CHECK(result->GetRPN() == "x ~");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(-5, 1e-5));
 
   CHECK(derivative->GetInfix(0) == "-1");
   CHECK(derivative->GetRPN() == "1 ~");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(-1, 1e-5));
 }
 
 TEST_CASE("-(x + 1)", "UnaryMinus") {
@@ -52,9 +57,11 @@ TEST_CASE("-(x + 1)", "UnaryMinus") {
 
   CHECK(result->GetInfix(0) == "-(x + 1)");
   CHECK(result->GetRPN() == "x 1 + ~");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(-6, 1e-5));
 
   CHECK(derivative->GetInfix(0) == "-(1 + 0)");
   CHECK(derivative->GetRPN() == "1 0 + ~");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(-1, 1e-5));
 }
 
 TEST_CASE("tan(1)", "Tangent") {
@@ -65,9 +72,11 @@ TEST_CASE("tan(1)", "Tangent") {
 
   CHECK(result->GetInfix(0) == "tan(1)");
   CHECK(result->GetRPN() == "1 tan");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::tan(1), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "0 / cos(1) ^ 2");
   CHECK(derivative->GetRPN() == "0 1 cos 2 ^ /");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinAbs(0, 1e-5));
 }
 
 TEST_CASE("tan(x)", "Tangent") {
@@ -78,9 +87,12 @@ TEST_CASE("tan(x)", "Tangent") {
 
   CHECK(result->GetInfix(0) == "tan(x)");
   CHECK(result->GetRPN() == "x tan");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::tan(5), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "1 / cos(x) ^ 2");
   CHECK(derivative->GetRPN() == "1 x cos 2 ^ /");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}),
+               Catch::Matchers::WithinRel(1 / (std::cos(5) * std::cos(5)), 1e-5));
 }
 
 TEST_CASE("tan(x + 25.3)", "Tangent") {
@@ -92,9 +104,12 @@ TEST_CASE("tan(x + 25.3)", "Tangent") {
 
   CHECK(result->GetInfix(0) == "tan(x + 25.3)");
   CHECK(result->GetRPN() == "x 25.3 + tan");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::tan(5 + 25.3), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "(1 + 0) / cos(x + 25.3) ^ 2");
   CHECK(derivative->GetRPN() == "1 0 + x 25.3 + cos 2 ^ /");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}),
+               Catch::Matchers::WithinRel(1 / (std::cos(5 + 25.3) * std::cos(5 + 25.3)), 1e-5));
 }
 
 TEST_CASE("tan(x) + 12", "Tangent") {
@@ -106,9 +121,12 @@ TEST_CASE("tan(x) + 12", "Tangent") {
 
   CHECK(result->GetInfix(0) == "tan(x) + 12");
   CHECK(result->GetRPN() == "x tan 12 +");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::tan(5) + 12, 1e-5));
 
   CHECK(derivative->GetInfix(0) == "1 / cos(x) ^ 2 + 0");
   CHECK(derivative->GetRPN() == "1 x cos 2 ^ / 0 +");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}),
+               Catch::Matchers::WithinRel(1 / (std::cos(5) * std::cos(5)), 1e-5));
 }
 
 TEST_CASE("sin(1)", "Sin") {
@@ -119,9 +137,11 @@ TEST_CASE("sin(1)", "Sin") {
 
   CHECK(result->GetInfix(0) == "sin(1)");
   CHECK(result->GetRPN() == "1 sin");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::sin(1), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "cos(1) * 0");
   CHECK(derivative->GetRPN() == "1 cos 0 *");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinAbs(0, 1e-5));
 }
 
 TEST_CASE("sin(x)", "Sin") {
@@ -132,9 +152,11 @@ TEST_CASE("sin(x)", "Sin") {
 
   CHECK(result->GetInfix(0) == "sin(x)");
   CHECK(result->GetRPN() == "x sin");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::sin(5), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "cos(x) * 1");
   CHECK(derivative->GetRPN() == "x cos 1 *");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::cos(5), 1e-5));
 }
 
 TEST_CASE("sin(x + 25.3)", "Sin") {
@@ -146,9 +168,11 @@ TEST_CASE("sin(x + 25.3)", "Sin") {
 
   CHECK(result->GetInfix(0) == "sin(x + 25.3)");
   CHECK(result->GetRPN() == "x 25.3 + sin");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::sin(5 + 25.3), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "cos(x + 25.3) * (1 + 0)");
   CHECK(derivative->GetRPN() == "x 25.3 + cos 1 0 + *");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::cos(5 + 25.3), 1e-5));
 }
 
 TEST_CASE("sin(x) + 12", "Sin") {
@@ -160,9 +184,11 @@ TEST_CASE("sin(x) + 12", "Sin") {
 
   CHECK(result->GetInfix(0) == "sin(x) + 12");
   CHECK(result->GetRPN() == "x sin 12 +");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::sin(5) + 12, 1e-5));
 
   CHECK(derivative->GetInfix(0) == "cos(x) * 1 + 0");
   CHECK(derivative->GetRPN() == "x cos 1 * 0 +");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::cos(5), 1e-5));
 }
 
 TEST_CASE("cos(1)", "Cos") {
@@ -173,9 +199,11 @@ TEST_CASE("cos(1)", "Cos") {
 
   CHECK(result->GetInfix(0) == "cos(1)");
   CHECK(result->GetRPN() == "1 cos");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::cos(1), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "-sin(1) * 0");
   CHECK(derivative->GetRPN() == "1 sin 0 * ~");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinAbs(0, 1e-5));
 }
 
 TEST_CASE("cos(x)", "Cos") {
@@ -186,9 +214,11 @@ TEST_CASE("cos(x)", "Cos") {
 
   CHECK(result->GetInfix(0) == "cos(x)");
   CHECK(result->GetRPN() == "x cos");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::cos(5), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "-sin(x) * 1");
   CHECK(derivative->GetRPN() == "x sin 1 * ~");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(-std::sin(5), 1e-5));
 }
 
 TEST_CASE("cos(x + 25.3)", "Cos") {
@@ -200,9 +230,11 @@ TEST_CASE("cos(x + 25.3)", "Cos") {
 
   CHECK(result->GetInfix(0) == "cos(x + 25.3)");
   CHECK(result->GetRPN() == "x 25.3 + cos");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::cos(5 + 25.3), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "-sin(x + 25.3) * (1 + 0)");
   CHECK(derivative->GetRPN() == "x 25.3 + sin 1 0 + * ~");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(-std::sin(5 + 25.3), 1e-5));
 }
 
 TEST_CASE("cos(x) + 12", "Cos") {
@@ -214,9 +246,11 @@ TEST_CASE("cos(x) + 12", "Cos") {
 
   CHECK(result->GetInfix(0) == "cos(x) + 12");
   CHECK(result->GetRPN() == "x cos 12 +");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::cos(5) + 12, 1e-5));
 
   CHECK(derivative->GetInfix(0) == "-sin(x) * 1 + 0");
   CHECK(derivative->GetRPN() == "x sin 1 * ~ 0 +");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(-std::sin(5), 1e-5));
 }
 
 TEST_CASE("ln(1)", "Logarithm") {
@@ -227,9 +261,11 @@ TEST_CASE("ln(1)", "Logarithm") {
 
   CHECK(result->GetInfix(0) == "ln(1)");
   CHECK(result->GetRPN() == "1 ln");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::log(1), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "0 / 1");
   CHECK(derivative->GetRPN() == "0 1 /");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinAbs(0, 1e-5));
 }
 
 TEST_CASE("ln(x)", "Logarithm") {
@@ -240,9 +276,11 @@ TEST_CASE("ln(x)", "Logarithm") {
 
   CHECK(result->GetInfix(0) == "ln(x)");
   CHECK(result->GetRPN() == "x ln");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::log(5), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "1 / x");
   CHECK(derivative->GetRPN() == "1 x /");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(0.2, 1e-5));
 }
 
 TEST_CASE("ln(x + 25.3)", "Logarithm") {
@@ -254,9 +292,11 @@ TEST_CASE("ln(x + 25.3)", "Logarithm") {
 
   CHECK(result->GetInfix(0) == "ln(x + 25.3)");
   CHECK(result->GetRPN() == "x 25.3 + ln");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::log(5 + 25.3), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "(1 + 0) / (x + 25.3)");
   CHECK(derivative->GetRPN() == "1 0 + x 25.3 + /");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(1 / (5 + 25.3), 1e-5));
 }
 
 TEST_CASE("ln(x) + 12", "Logarithm") {
@@ -268,9 +308,11 @@ TEST_CASE("ln(x) + 12", "Logarithm") {
 
   CHECK(result->GetInfix(0) == "ln(x) + 12");
   CHECK(result->GetRPN() == "x ln 12 +");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::log(5) + 12, 1e-5));
 
   CHECK(derivative->GetInfix(0) == "1 / x + 0");
   CHECK(derivative->GetRPN() == "1 x / 0 +");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(0.2, 1e-5));
 }
 
 TEST_CASE("sqrt(1)", "SquareRoot") {
@@ -281,9 +323,11 @@ TEST_CASE("sqrt(1)", "SquareRoot") {
 
   CHECK(result->GetInfix(0) == "sqrt(1)");
   CHECK(result->GetRPN() == "1 sqrt");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::sqrt(1), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "0 / (2 * sqrt(1))");
   CHECK(derivative->GetRPN() == "0 2 1 sqrt * /");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(1 / (5), 1e-5));
 }
 
 TEST_CASE("sqrt(x)", "SquareRoot") {
@@ -294,9 +338,11 @@ TEST_CASE("sqrt(x)", "SquareRoot") {
 
   CHECK(result->GetInfix(0) == "sqrt(x)");
   CHECK(result->GetRPN() == "x sqrt");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::sqrt(5), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "1 / (2 * sqrt(x))");
   CHECK(derivative->GetRPN() == "1 2 x sqrt * /");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(1 / (2 * sqrt(5)), 1e-5));
 }
 
 TEST_CASE("sqrt(x + 25.3)", "SquareRoot") {
@@ -308,9 +354,11 @@ TEST_CASE("sqrt(x + 25.3)", "SquareRoot") {
 
   CHECK(result->GetInfix(0) == "sqrt(x + 25.3)");
   CHECK(result->GetRPN() == "x 25.3 + sqrt");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::sqrt(5 + 25.3), 1e-5));
 
   CHECK(derivative->GetInfix(0) == "(1 + 0) / (2 * sqrt(x + 25.3))");
   CHECK(derivative->GetRPN() == "1 0 + 2 x 25.3 + sqrt * /");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(1 / (2 * sqrt(5 + 25.3)), 1e-5));
 }
 
 TEST_CASE("sqrt(x) + 12", "SquareRoot") {
@@ -322,7 +370,9 @@ TEST_CASE("sqrt(x) + 12", "SquareRoot") {
 
   CHECK(result->GetInfix(0) == "sqrt(x) + 12");
   CHECK(result->GetRPN() == "x sqrt 12 +");
+  REQUIRE_THAT(result->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(std::sqrt(5) + 12, 1e-5));
 
   CHECK(derivative->GetInfix(0) == "1 / (2 * sqrt(x)) + 0");
   CHECK(derivative->GetRPN() == "1 2 x sqrt * / 0 +");
+  REQUIRE_THAT(derivative->GetNumericResult({{'x', 5}}), Catch::Matchers::WithinRel(1 / (2 * sqrt(5)), 1e-5));
 }
